96d234aeb89755c60ff36c4caae6038c8c95cfd3
b1t-ninja <nrueckner@outlook.de>
2024-12-30 14:07:31 +0100

    Updated HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,8 +5,8 @@ export default function HeroSection() {
   return <div className={"flex flex-col items-center justify-center mt-20 space-y-4 md:space-y-8"}>
     <h1 className={'font-bold text-3xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl'}>Hey there ✨</h1>
     <h2 className={'text-l sm:text-2xl md:text-4xl lg:text:5xl xl:text:5xl'}>I am a <span
-      className={'text-[var(--fg-blue)]'}>fullstack</span> software engineer based in <span>Berlin</span><br/></h2>
-    <h2 className={'text-l sm:text-2xl md:text-4xl lg:text:5xl xl:text:5xl'}>Mostly dealing with <span
+      className={'text-[var(--fg-blue)]'}>CS Student</span> based in <span>Berlin</span><br/></h2>
+    <h2 className={'text-l sm:text-2xl md:text-4xl lg:text:5xl xl:text:5xl'}>Mostly using <span
       className={'text-[var(--fg-orange)]'}>SwiftUI</span> and <span className='text-[var(--fg-gray)]'>NextJS</span>
     </h2>
     <hr/>
